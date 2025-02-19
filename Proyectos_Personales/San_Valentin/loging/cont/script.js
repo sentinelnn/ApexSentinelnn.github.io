2f80308ec8a0668d3eba1f02a6d7178f6b9be42b
sentinelnn <187855@upslp.edu.mx>
2025-02-19 13:06:38 +0000

    delete archives

--- Proyectos_Personales/San_Valentin/loging/cont/script.js
+++
@@ -1,6 +0,0 @@
-// Ejemplo de interacción: Cambiar el color del título al hacer clic
-const title = document.querySelector('.title');
-
-title.addEventListener('click', () => {
-    title.style.color = '#ff7eb3'; // Cambia el color del título al hacer clic
-});
